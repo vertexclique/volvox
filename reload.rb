c77b08ca3e749640aa7e5c251a40496d18cfebb2
Mahmut Bulut <mahmutbulut0@gmail.com>
2012-06-16 16:26:55 +0300

    fixes

--- reload.rb
+++ reload.rb
@@ -5,21 +5,24 @@
 File.open('local.el', 'w') do |f|
   f.puts '(load "volvox")'
   f.puts '(autoload \'markdown-mode "markdown-mode.el"
-  				"Major mode for editing Markdown files" t)
-		  (setq auto-mode-alist
-				(cons \'("\\.md" . markdown-mode) auto-mode-alist))'
+  				  "Major mode for editing Markdown files" t)
+		      (setq auto-mode-alist
+				    (cons \'("\\.md" . markdown-mode) auto-mode-alist))'
   f.puts '(require \'mustache-mode)'
   f.puts '(require \'any-ini-mode)'
   f.puts '(require \'color-theme)'
   f.puts '(autoload \'js2-mode "js2" nil t)
-  		  (add-to-list \'auto-mode-alist \'("\\.js$" . js2-mode))'
+  		      (add-to-list \'auto-mode-alist \'("\\.js$" . js2-mode))'
   f.puts '(require \'magit)'
   f.puts '(autoload \'mode-compile "mode-compile"
-		   "Command to compile current buffer file based on the major mode" t)'
+		        "Command to compile current buffer file based on the major mode" t)'
   f.puts '(autoload \'scss-mode "scss-mode")
-  		  (add-to-list \'auto-mode-alist \'("\\.scss\\\'" . scss-mode))'
+  		      (add-to-list \'auto-mode-alist \'("\\.scss\\\'" . scss-mode))'
   f.puts '(require \'textmate)
-          (textmate-mode)'
+            (textmate-mode)'
+  f.puts '(require \'yaml-mode)
+            (add-to-list \'auto-mode-alist \'("\\.yml$" . yaml-mode))'
+  
 end
 
 puts "Don't forget to M-x byte-compile-file js2"
